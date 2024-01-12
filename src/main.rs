b25b8e34b3777c1ce64354331c0aa9f5415dbc07
Kazz2433 <kelvinquida24@gmail.com>
2024-01-12 01:38:57 -0400

    git add: comparign numbers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
 
     println!("Please input your guess!");
 
@@ -10,6 +16,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    
-    println!("You guessed:{guess}")
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guessed:{guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
+    }
 }
